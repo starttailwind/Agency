c5e8e490c13e3792a9fd781680105dea9a8e9298
ayushAB <ayushbante6@gmail.com>
2021-12-03 18:37:44 +0530

    added header layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+ <header class="flex justify-between px-8 py-4 sticky z-10">
+   <div>
+     <h1>Start Tailwindcss</h1>
+   </div>
+   <ul class="flex justify-around">
+     <li class="m-2">Services</li>
+     <li class="m-2">Portfolio</li>
+     <li class="m-2">About</li>
+     <li class="m-2">Team</li>
+     <li class="m-2">Contact</li>
+   </ul>
+  
+ </header>
 </template>
